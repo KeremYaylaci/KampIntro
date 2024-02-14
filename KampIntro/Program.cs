3061b89d7f3c0614428cb2d2e70679bfeeff5208
k.yaylaci <k.yaylaci@KYAYLACI>
2024-02-14 17:34:32 +0300

    Proje dosyası ekle.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Transactions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        string kategoriEtiketi = "kategoriler";
+        int ogrenciSayisi = 12334;
+        double faizOrani = 1.45;
+        bool sistemeGirişYapmisMi = true;
+        double dolarDun = 7.35;
+        double dolarBugun = 7.45;
+
+        if (dolarDun>dolarBugun)
+        {
+            Console.WriteLine("azalış butonu");
+        }
+
+        else if (dolarDun<dolarBugun)
+                {
+            Console.WriteLine("artış butonu");
+        }
+        else
+        {
+            Console.WriteLine("değişmedi butonu");
+        }
+
+        if (sistemeGirişYapmisMi == true)
+        {
+            Console.WriteLine("Kullanıcı Ayarları Butonu");
+
+        }
+        else
+        {
+            Console.WriteLine("Giriş Yap Butonu");
+        }
+       
+
+
+
+    }
+}
